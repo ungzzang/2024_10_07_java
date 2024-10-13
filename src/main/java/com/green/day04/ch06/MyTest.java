d78a11afb0b01b996a02cc1e125f9007996bbd82
User <wodnd1998@gmail.com>
2024-10-13 20:55:50 +0900

    10-13 정리.

--- src/main/java/com/green/day04/ch06/MyTest.java
+++ src/main/java/com/green/day04/ch06/MyTest.java
@@ -1,41 +1,25 @@
 package com.green.day04.ch06;
+
 import java.util.Scanner;
 
 public class MyTest {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
-            System.out.print("행 수를 입력하세요: ");
-            int rows = scanner.nextInt(); // 사용자가 입력한 행 수
+        int star = (int)(Math.random()*6+2);
+        System.out.println("star = " + star);
 
-            // 상단 삼각형
-            for (int i = 1; i <= rows; i++) {
-                // 공백 출력
-                for (int j = i; j < rows; j++) {
-                    System.out.print(" "); // 공백
+        int i, z;
+
+        for(i = star; i >0; i--) {
+            for(z = 1; z <= star; z++){
+                if(i > z) {
+                    System.out.print("_");
+                } else {
+                    System.out.print("*");
                 }
-                // 별 출력
-                for (int j = 1; j <= (2 * i - 1); j++) {
-                    System.out.print("*"); // 별
-                }
-                System.out.println(); // 다음 줄로 이동
             }
-
-            // 하단 삼각형
-            for (int i = rows - 1; i >= 1; i--) {
-                // 공백 출력
-                for (int j = rows; j > i; j--) {
-                    System.out.print(" "); // 공백
-                }
-                // 별 출력
-                for (int j = 1; j <= (2 * i - 1); j++) {
-                    System.out.print("*"); // 별
-                }
-                System.out.println(); // 다음 줄로 이동
-            }
-
-            scanner.close(); // Scanner 종료
+            System.out.println();
         }
 
-
+    }
 }
