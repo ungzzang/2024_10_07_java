1022a13edd0f44b4c3a0cc5d0c2aaadfab1c9b37
jangjaeung <wodnd1998@gmail.com>
2024-11-07 17:58:39 +0900

    24.11.07 database [DB통신]

---
+++ src/main/java/com/green/database/Board.java
@@ -0,0 +1,59 @@
+package com.green.database;
+
+public class Board {
+    private int boardId;
+    private String title;
+    private String contents;
+    private String writer;
+    private String createdAt;
+    private String updatedAt;
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+
+    public void setBoardId(int boardId) {
+        this.boardId = boardId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/com/green/database/BoardDao.java
@@ -0,0 +1,44 @@
+package com.green.database;
+
+import java.sql.*;
+
+//Data Access Object (DB 작업하는 객체)
+public class BoardDao {
+
+    public int insBoard(Board board) {
+        int result = 0;
+        String sql = " INSERT INTO board " +
+                        " SET title = ? " +
+                        "   , contents = ? " +
+                        "   , writer = ? ";
+
+        try (Connection conn = MyConnection.getConn()
+            ; PreparedStatement ps = conn.prepareStatement(sql)) {
+            // ?부분에 값 채우기
+            ps.setString(1, board.getTitle());
+            ps.setString(2, board.getContents());
+            ps.setString(3, board.getWriter());
+
+            result = ps.executeUpdate(); //쿼리 실행
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public int delBoard(int boardId) {
+        int result = 0;
+        String sql = " DELETE FROM board WHERE board_id = ? ";
+        try (Connection conn = MyConnection.getConn()
+             ; PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, boardId);
+            result = ps.executeUpdate();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+}
